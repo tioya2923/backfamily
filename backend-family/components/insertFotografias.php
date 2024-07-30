c7deafc01c14c47dd5ff1f9c165ff5a6bfa609d4
tioya2923 <30002076@students.ual.pt>
2024-07-30 15:37:51 +0100

    fotoVid

--- backend-family/components/insertFotografias.php
+++ backend-family/components/insertFotografias.php
@@ -2,34 +2,25 @@
 require_once '../connect/server.php';
 require_once '../connect/cors.php';
 
-// Certifique-se de que o método de solicitação é POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Escapar entradas para prevenir SQL Injection
     $nome = mysqli_real_escape_string($conn, $_POST['nome']);
     $descricao = mysqli_real_escape_string($conn, $_POST['descricao']);
 
-    // Verificar se o arquivo foi enviado sem erros
     if (isset($_FILES['image']) && $_FILES['image']['error'] === 0) {
         $image = $_FILES['image'];
         $filename = mysqli_real_escape_string($conn, $image['name']);
-        $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/uploadsFotos/";
+        $target_dir = __DIR__ . '/../uploadsFotos/';
 
-        // Verificar se o arquivo é uma imagem
         if (getimagesize($image['tmp_name']) !== false) {
-            // Mover o arquivo enviado para o diretório de destino
             if (move_uploaded_file($image['tmp_name'], $target_dir . $filename)) {
-                // Preparar a consulta SQL para evitar SQL Injection
                 $stmt = $conn->prepare("INSERT INTO fotos (nome, foto, descricao) VALUES (?, ?, ?)");
                 $stmt->bind_param("sss", $nome, $filename, $descricao);
 
-                // Executar a consulta e verificar se foi bem-sucedida
                 if ($stmt->execute()) {
                     echo "Foto enviada com sucesso!";
                 } else {
                     echo "Erro ao enviar a foto: " . $stmt->error;
                 }
-
-                // Fechar a declaração preparada
                 $stmt->close();
             } else {
                 echo "Ocorreu um erro ao mover a foto.";
@@ -44,6 +35,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "Método de solicitação inválido.";
 }
 
-// Fechar a conexão
 $conn->close();
 ?>
--- backend-family/components/insertVideos.php
+++ backend-family/components/insertVideos.php
@@ -1,44 +1,30 @@
 <?php
-// Incluir o ficheiro de conexão
 require_once '../connect/server.php';
 require_once '../connect/cors.php';
 
-// Incluir a biblioteca do api.video
-require_once '../vendor/autoload.php';
-
-// Certifique-se de que o método de solicitação é POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome = mysqli_real_escape_string($conn, $_POST['nome']);
     $descricao = mysqli_real_escape_string($conn, $_POST['descricao']);
 
-    // Verifica se algum arquivo foi enviado e se não há erros
     if (isset($_FILES['video']) && $_FILES['video']['error'] == 0) {
         $video = $_FILES['video'];
         $filename = mysqli_real_escape_string($conn, $video['name']);
-        $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/uploadsVideos/";
-        $max_size = 1024 * 1024 * 1024; // Tamanho máximo do arquivo
-        $allowed_exts = array('mp4', 'mov', 'avi', 'mkv', 'webm'); // Extensões permitidas
+        $target_dir = __DIR__ . '/../uploadsVideos/';
+        $max_size = 1024 * 1024 * 1024;
+        $allowed_exts = array('mp4', 'mov', 'avi', 'mkv', 'webm');
         $size = filesize($video['tmp_name']);
         $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
-        // Se o tamanho e a extensão forem válidos, move o arquivo para a pasta
         if ($size <= $max_size && in_array($ext, $allowed_exts)) {
             if (move_uploaded_file($video['tmp_name'], $target_dir . $filename)) {
-                // Preparar a consulta SQL para evitar SQL Injection
                 $stmt = $conn->prepare("INSERT INTO videos (nome, video, descricao) VALUES (?, ?, ?)");
                 $stmt->bind_param('sss', $nome, $filename, $descricao);
 
-                // Executar a consulta e verificar se foi bem-sucedida
                 if ($stmt->execute()) {
-                    // Código para enviar o vídeo para o api.video
-                    // ...
-
                     echo "Vídeo enviado com sucesso!";
                 } else {
                     echo "Ocorreu um erro ao enviar o vídeo: " . $stmt->error;
                 }
-
-                // Fechar a declaração preparada
                 $stmt->close();
             } else {
                 echo "Ocorreu um erro ao mover o vídeo.";
@@ -53,6 +39,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "Método de solicitação inválido.";
 }
 
-// Fechar a conexão
 $conn->close();
 ?>
