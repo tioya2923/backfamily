12c865e702e89cb5d1917822d0017f504594f3b1
tioya2923 <30002076@students.ual.pt>
2024-08-05 20:49:22 +0100

    variáveis459

--- backend-family/connect/cors.php
+++ backend-family/connect/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 $origensPermitidas = [
-    'http://localhost:3000/backend-family',
-    'https://frontend-family-b1523b1674d4.herokuapp.com/backend-family',
-    'https://familiagouveia.pt/backend-family'
+    'http://localhost:3000',
+    'https://frontend-family-b1523b1674d4.herokuapp.com',
+    'https://familiagouveia.pt'
    
  
     ];
